d612f42c52c4330dae529c5bb4daf2d152d617cb
Ben M <benmag@users.noreply.github.com>
2018-08-02 21:13:15 +1000

    Implement Shopify auth view

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,12 +2,54 @@
 
 @section('content')
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+
+    <div class="row justify-content-center mt-5">
+        <div class="col-md-4">
+            <div class="card">
+                <div class="card-body">
+                    <div class="text-center">
+                        <h3>Create a new account</h3>
+                        <p class="text-muted">Get our 30-day free trial and start increasing your sales today</p>
+                    </div>
+                    <hr class="mb-4">
+                    <form method="GET" action="{{ route('login.shopify') }}" aria-label="{{ __('Register') }}">
+                        <div class="form-group">
+                            <label for="domain">Domain</label>
+
+                            <div class="input-group mb-3">
+                                <input id="domain" type="text" class="form-control{{ $errors->has('domain') ? ' is-invalid' : '' }}" name="domain" value="{{ old('domain') }}" placeholder="yourshop" aria-describedby="myshopify" required autofocus>
+                                <div class="input-group-append">
+                                    <span class="input-group-text" id="myshopify">myshopify.com</span>
+                                </div>
+                            </div>
+
+                            <button type="submit" class="btn btn-primary btn-block">Continue</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+
+            <div class="text-center mt-3">
+                <p class="text-center text-muted">Already have an account? <a href="{{ route('login') }}">Sign in here</a></p>
+            </div>
+        </div>
+    </div>
+
+    <div class="row justify-content-center" hidden>
+        <div class="col-md-6">
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
                 <div class="card-body">
+
+                    {{--<div class="row">--}}
+                        {{--<div class="col-md-4">--}}
+                            {{--<div class="card socialite-card shopify">--}}
+                                {{--<div class="card-body">Shopify</div>--}}
+                            {{--</div>--}}
+                        {{--</div>--}}
+                    {{--</div>--}}
+
                     <form method="POST" action="{{ route('register') }}" aria-label="{{ __('Register') }}">
                         @csrf
 
