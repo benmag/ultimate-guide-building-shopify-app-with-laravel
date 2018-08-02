fdd150aabca2d9d893db59a2bc4fc1d8ea09235f
Ben M <benmag@users.noreply.github.com>
2018-08-02 21:14:04 +1000

    Stub `VerifyUserIsSubscribed` middleware

---
+++ app/Http/Middleware/VerifyUserIsSubscribed.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Middleware;
+
+class VerifyUserIsSubscribed
+{
+    /**
+     * Verify the incoming request's user has a subscription.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @param  string  $subscription
+     * @param  string  $plan
+     * @return \Illuminate\Http\Response
+     */
+    public function handle($request, $next, $subscription = 'default', $plan = null)
+    {
+        if ($this->subscribed($request->user(), $subscription, $plan, func_num_args() === 2)) {
+            return $next($request);
+        }
+
+        return redirect()
+        return $request->ajax() || $request->wantsJson()
+            ? response('Subscription Required.', 402)
+            : redirect('/settings#/subscription');
+    }
+
+    /**
+     * Determine if the given user is subscribed to the given plan.
+     *
+     * @param  \Illuminate\Contracts\Auth\Authenticatable  $user
+     * @param  string  $subscription
+     * @param  string  $plan
+     * @param  bool  $defaultSubscription
+     * @return bool
+     */
+    protected function subscribed($user, $subscription, $plan, $defaultSubscription)
+    {
+        return false;
+
+        if (! $user) {
+            return false;
+        }
+
+        return ($defaultSubscription && $user->onGenericTrial()) ||
+        $user->subscribed($subscription, $plan);
+    }
+}
