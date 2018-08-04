b92757d78c2626e5ae4d19122d8954eb8c070e9f
Ben M <benmag@users.noreply.github.com>
2018-08-04 18:23:44 +1000

    User providers

---
+++ app/UserProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class UserProvider extends Model
+{
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id', 'provider', 'provider_user_id', 'access_token',
+    ];
+
+}
---
+++ database/migrations/2018_08_03_173119_create_user_providers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserProvidersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('user_providers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('user_id')->unsigned();
+            $table->string('provider');
+            $table->string('provider_user_id');
+            $table->string('access_token');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('user_providers');
+    }
+}
