c8ecb1fc4f793a145be940db1e35f5c8bf59b367
Ben M <benmag@users.noreply.github.com>
2018-08-05 18:20:25 +1000

    Refactor: Shop to Stores

--- database/migrations/2018_08_03_162001_create_stores_table.php
+++ database/migrations/2018_08_03_162001_create_stores_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateShopsTable extends Migration
+class CreateStoresTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_08_03_173052_create_store_users_table.php
+++ database/migrations/2018_08_03_173052_create_store_users_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateShopUsersTable extends Migration
+class CreateStoreUsersTable extends Migration
 {
     /**
      * Run the migrations.
