b21a7fe28494cea2715b34f395b7218cf3a4df6b
Ben M <benmag@users.noreply.github.com>
2018-08-06 10:48:34 +1000

    Add `subscribed` middleware to HomeController

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Store;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -13,7 +14,7 @@ class HomeController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware(['auth', 'subscribed']);
     }
 
     /**
