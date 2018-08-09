23c88c9e2e0c1238edd2d4f13933d83ada5db210
Ben M <benmag@users.noreply.github.com>
2018-08-09 11:26:18 +1000

    Webhooks & update route to have storeId as a route param

--- routes/web.php
+++ routes/web.php
@@ -21,10 +21,11 @@ Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('login/shopify', 'Auth\LoginShopifyController@redirectToProvider')->name('login.shopify');
 Route::get('login/shopify/callback', 'Auth\LoginShopifyController@handleProviderCallback');
-Route::get('shopify/subscribe', function(\Illuminate\Http\Request $request) {
+
+Route::get('stores/{storeId}/shopify/subscribe', function(\Illuminate\Http\Request $request, $storeId) {
 
 
-    $store = \App\Store::find(1);
+    $store = \App\Store::find($storeId);
     $user = auth()->user()->providers->where('provider', 'shopify')->first();
     $shopify = \Shopify::retrieve($store->domain, $user->provider_token);
 
@@ -32,8 +33,6 @@ Route::get('shopify/subscribe', function(\Illuminate\Http\Request $request) {
         ->activate($store->domain, $user->provider_token, $request->get('charge_id'));
 
     $response = array_get($activated->getActivated(), 'recurring_application_charge');
-
-    $store = \App\Store::find(1);
 
     \App\Charge::create([
         'store_id' => $store->id,
@@ -49,3 +48,19 @@ Route::get('shopify/subscribe', function(\Illuminate\Http\Request $request) {
     return redirect('/home');
 
 })->name('shopify.subscribe');
+
+Route::post('webhook/shopify/uninstall', function(\Illuminate\Http\Request $request) {
+    // Handle app uninstall
+})->middleware('webhook');
+
+Route::post('webhook/shopify/gdpr/customer-redact', function(\Illuminate\Http\Request $request) {
+    // Remove customer data
+})->middleware('webhook');
+
+Route::post('webhook/shopify/gdpr/shop-redact', function(\Illuminate\Http\Request $request) {
+    // Remove shop data
+})->middleware('webhook');
+
+Route::post('webhook/shopify/gdpr/customer-data', function(\Illuminate\Http\Request $request) {
+    // Provide data on customer
+})->middleware('webhook');
